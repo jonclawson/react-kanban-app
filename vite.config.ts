9a9f7e98fcae2d1118974d1a90abea162d80da85
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-06-04 16:40:09 -0700

    ci tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,9 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
 })
